c1f4e915f615342e1e7ecce677aea51ef0a6c26c
Gossepojke <svedlund.oskar@gmail.com>
2022-08-18 20:03:57 +0200

    removed comments on Bijele.

--- Bijele.py
+++ Bijele.py
@@ -1,19 +1,3 @@
-# total = input()
-
-# king, queen, rook, bishop, knight, pawns = total.split(" ")
-
-
-
-# if king == 0:
-#     print("0")
-
-# elif king > 1:
-#     print(king - 1)
-
-# elif king < 1:
-#     print()
-
-
 list = input()
 
 needed = [1, 1, 2, 2, 2, 8]
@@ -24,22 +8,3 @@ for i in range(len(needed)):
     difference.append(needed[i] - have[i])
 
 print(" ".join([str(x) for x in difference]))
-
-# import sys
-
-# inputstd = sys.stdin.read()
-
-# correct = [1, 1, 2, 2, 2, 8]
-
-# line = inputstd.strip("\n")
-# values = line.split(" ")
-
-# i = 0
-# correction = ""
-
-# for item in values:
-# 	item = int(item)
-# 	correction += str((correct[i] - item)) + " "
-# 	i += 1
-	
-# print (correction)
